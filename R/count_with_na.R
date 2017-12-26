1d4c195293c6841b4db743cf23d789f2753ea499
vincent <vincent@thinkr.fr>
2017-12-26 11:46:32 +0100

    test format

--- R/count_with_na.R
+++ R/count_with_na.R
@@ -26,3 +26,54 @@ count_with_na <- function(x,...){
     # mutate(pct= glue("{round(pct,2)} %")) %>% # trouver dans arsenal::
     tibble::column_to_rownames("x")
 }
+
+
+
+#' Title
+#'
+#' @param x
+#' @param digits
+#' @param pct
+#'
+#' @return
+#' @export
+#'
+#' @examples
+format.countpct <- function(x,digits=5, pct='') {
+  if(!is.null(ncol(x))) {
+    ## multiple rows
+    xformat <- cbind.data.frame(format(x[,1], digits=digits), format(x[,2], digits=digits))
+    row.names(xformat) <- row.names(x)
+    digits <- digits - 2
+    return (apply (xformat, 1, function(xrow) paste (xrow[1], " (", format (round (as.numeric (xrow[2]), digits), nsmall = digits),
+                                                     pct, ")", sep = "")))
+  } else {
+    ## just one row
+    return(paste(signif(x[1],digits=digits), "(",signif(x[2],digits=digits), ")",sep=""))
+  }
+}
+
+
+#' Title
+#'
+#' @param x
+#' @param digits
+#' @param pct
+#'
+#' @return
+#' @export
+#'
+#' @examples
+format.count_with_na <- function(x,digits=5, pct='') {
+  if(!is.null(ncol(x))) {
+    ## multiple rows
+    xformat <- cbind.data.frame(format(x[,1], digits=digits), format(x[,2], digits=digits))
+    row.names(xformat) <- row.names(x)
+    digits <- digits - 2
+    return (apply (xformat, 1, function(xrow) paste (xrow[1], " (", format (round (as.numeric (xrow[2]), digits), nsmall = digits),
+                                                     pct, ")", sep = "")))
+  } else {
+    ## just one row
+    return(paste(signif(x[1],digits=digits), "(",signif(x[2],digits=digits), ")",sep=""))
+  }
+}

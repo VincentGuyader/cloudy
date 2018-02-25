e4357eb970ff03cde8cf79d1fa0b037cb6d99029
vincent <vincent@thinkr.fr>
2018-02-25 23:21:08 +0100

    avec camembert

--- R/dessin_quali.R
+++ R/dessin_quali.R
@@ -24,10 +24,11 @@ ggplot(aes(x = Var, y = Freq)) +
 }
 
 
-dessin_quali <- function(var,nom){
+dessin_quali <- function(var,nom,camembert = FALSE){
   if (missing(nom)){nom <- deparse(substitute(var))}
 
-  var %>%
+  if ( !camembert ){
+  out <- var %>%
   table() %>%
   as.data.frame() %>%
   setNames(c("Var","Freq")) %>%
@@ -35,6 +36,32 @@ ggplot(aes(x = Var, y = Freq)) +
   geom_bar(stat = "identity", color = "black", fill = "grey") +
   labs(title = "", x = paste("\n",nom), y = "Frequence\n") +
   theme_classic()
+}
+  if ( camembert ){
+
+    out <-
+      var %>%
+      table() %>%
+      as.data.frame() %>%
+      setNames(c("Var","Freq")) %>%
+      ggplot( aes(x=factor(1), fill=Var))+
+      geom_bar(width = 1)+
+      coord_polar("y") +
+      # theme_classic()+
+      theme(axis.text.x=element_blank()) +
+      theme(plot.background = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.border = element_blank(),
+        panel.grid=element_blank(),
+        axis.ticks = element_blank(),
+        plot.title=element_text(size=14, face="bold")
+      )+labs(Var="coucou")+
+      theme(rect = element_rect(fill = NA,
+                               size = 12, colour = NA, linetype = 0), panel.background = element_rect(fill = NA))+
+        scale_fill_discrete(name = "")+ggtitle(nom)
+  }
+  out
 }
 
 
@@ -48,18 +75,17 @@ ggplot(aes(x = Var, y = Freq)) +
 #' @examples
 #' dessin_quali_all(iris)
 #'
-dessin_quali_all <- function(dataset,groupe,tidy=TRUE){
+dessin_quali_all <- function(dataset,groupe,tidy=TRUE,camembert=FALSE){
 
   if ( tidy ){
-    dataset <- dataset%>% rename_all(~str_replace_all(.,"_"," "))
-
+    dataset <- dataset %>% rename_all(~str_replace_all(.,"_"," "))
   }
 
   if ( is_missing(groupe)){
 
 out <-  dataset %>%
     select_if(is.factor) %>%
-    map2(.,names(.),dessin_quali)
+    map2(.,names(.),dessin_quali,camembert=camembert)
 }
   if ( !is_missing(groupe)){
 
